f058dc6c9e9b85184450fadb4e341766dca7a7d8
Blaine Malone <blainemalone01@gmail.com>
2023-09-23 15:28:31 -0400

    fix: using deploy scriopt

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -2,29 +2,46 @@ import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { deployer } = await hre.getNamedAccounts();
-  const { deploy } = hre.deployments;
-
-  console.log(deployer);
-
-  const greeter = await deploy("PaymasterNft", {
-    from: deployer,
-    args: [],
-    log: true,
-  });
-
-  console.log(`Greeter contract: `, greeter.address);
-
+  const signers = await hre.ethers.getSigners();
+  /**
+   * Deploying Paymaster
+   */
+  const nftGatedPaymasterFactory = await hre.ethers.getContractFactory("NftGatedPaymaster");
   const entryPointContract = "0x5ff137d4b0fdcd49dca30c7cf57e578a026d2789";
+  const nftGatedPAymaster = await nftGatedPaymasterFactory.connect(signers[0]).deploy(entryPointContract);
+  await nftGatedPAymaster.waitForDeployment();
+  console.log("NftGatedPAymaster deployed to: ", await nftGatedPAymaster.getAddress());
 
-  const nftGatedPaymaster = await deploy("NftGatedPaymaster", {
-    from: deployer,
-    args: [entryPointContract],
-    log: true,
-  })
+  const addressCount = await nftGatedPAymaster.getAddressCount();
+  console.log(addressCount);
 
-  console.log(`NftGatedPaymaster contract: `, nftGatedPaymaster.address);
+  await nftGatedPAymaster.addNFTCollection("0xcafebabeb0fdcd49dca30c7cf57e578a026d2789");
+
+  const addressCountTwo = await nftGatedPAymaster.getAddressCount();
+  console.log(addressCountTwo);
+
+  /**
+   * Deploying NFT contracts
+   */
+  const eipRenderFactory = await hre.ethers.getContractFactory("EIPRender");
+  const eipRender = await eipRenderFactory.connect(signers[0]).deploy();
+
+  // //  console.log(`The address the Contract (EIPRender) WILL have once mined: ${eipRender.address}`);
+  // //  console.log(`The transaction that was sent to the network to deploy the Contract: ${eipRender.deployTransaction.hash}`);
+  // //  console.log("The contract is NOT deployed yet; we must wait until it is mined...");
+  await eipRender.waitForDeployment();
+  //  console.log("EIPRender Mined!");
+
+  const eipRenderAddress = await eipRender.getAddress()
+  const eipNftFactory = await hre.ethers.getContractFactory("EIPNFT", {
+    libraries: {
+      EIPRender: eipRenderAddress,
+    },
+  });
+  const eipNFT = await eipNftFactory.connect(signers[0]).deploy(signers[0].address, 1000);
+  const eipNFTAddress = await eipNFT.getAddress();
+  console.log(eipNFTAddress);
 };
 export default func;
-func.id = "deploy_greeter"; // id required to prevent reexecution
-func.tags = ["Greeter"];
+func.id = "deploy_contracts"; // id required to prevent reexecution
+func.tags = [""];
