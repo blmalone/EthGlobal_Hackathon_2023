c804423555c5de41265b2d2e730c8e46c29ca79b
Aris Agdere <aagdere1@gmail.com>
2023-09-23 13:13:32 -0400

    Frontend boilerplate

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './Login.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Login />
+      </header>
+    </div>
+  );
+}
+
+export default App;
