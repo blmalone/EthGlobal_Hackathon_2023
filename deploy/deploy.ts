277f67fee6236326822282de6c4757811766ac90
Blaine Malone <blainemalone01@gmail.com>
2023-09-23 17:42:25 -0400

    fix: deploying to polygon

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -1,85 +1,103 @@
 import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { waitForDebugger } from "inspector";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const signers = await hre.ethers.getSigners();
 
+  console.log(`\n############################ `);
+  console.log(`######### Gas Pass #########`);
+  console.log(`############################ `);
+  const signers = await hre.ethers.getSigners();
+  console.log(`Deployer Address: ${signers[0].address}`);
+  console.log(`Starting balance: ${await hre.ethers.provider.getBalance(signers[0].address)}`);
   /**
    * Deploying Paymaster
    */
   const nftGatedPaymasterFactory = await hre.ethers.getContractFactory("NftGatedPaymaster");
   const entryPointContract = "0x5ff137d4b0fdcd49dca30c7cf57e578a026d2789";
-  const nftGatedPAymaster = await nftGatedPaymasterFactory.connect(signers[0]).deploy(entryPointContract);
-  await nftGatedPAymaster.waitForDeployment();
-  console.log("NftGatedPAymaster deployed to: ", await nftGatedPAymaster.getAddress());
+  const nftGatedPaymaster = await nftGatedPaymasterFactory.connect(signers[0]).deploy(entryPointContract);
+  console.log("NftGatedPaymaster deployed to: ", await nftGatedPaymaster.getAddress());
 
-  const addressCount = await nftGatedPAymaster.getAddressCount();
+  await wait(5, hre);
 
-  await nftGatedPAymaster.addNFTCollection("0xcafebabeb0fdcd49dca30c7cf57e578a026d2789");
-
-  const addressCountTwo = await nftGatedPAymaster.getAddressCount();
-  console.log("Number of NFT collections registered to paymaster: ", addressCountTwo);
+  const addressCount = await nftGatedPaymaster.getAddressCount();
+  console.log("Number of NFT collections registered to paymaster: ", addressCount);
 
   /**
    * Deploying NFT contracts
    */
   const eipRenderFactory = await hre.ethers.getContractFactory("EIPRender");
   const eipRender = await eipRenderFactory.connect(signers[0]).deploy();
+  await wait(5, hre);
 
-  await eipRender.waitForDeployment();
+  const eipRenderAddress = await eipRender.getAddress();
+  console.log(`NFT Render contract deployed: ${eipRenderAddress}`);
+  // const eipNftFactory = await hre.ethers.getContractFactory("EIPNFT", {
+  //   libraries: {
+  //     EIPRender: eipRenderAddress,
+  //   },
+  // });
+  // const paymasterAddress = await nftGatedPaymaster.getAddress();
+  // const eipNFT = await eipNftFactory.connect(signers[0]).deploy(signers[0].address, 1000, paymasterAddress);
+  // const eipNFTAddress = await eipNFT.getAddress();
+  // await nftGatedPaymaster.addNFTCollection(eipNFTAddress);
+  // console.log("NFT address: ", eipNFTAddress);
+  // const addressCountTwo = await nftGatedPaymaster.getAddressCount();
+  // console.log("Number of NFT collections registered to paymaster: ", addressCountTwo);
 
-  const eipRenderAddress = await eipRender.getAddress()
-  const eipNftFactory = await hre.ethers.getContractFactory("EIPNFT", {
-    libraries: {
-      EIPRender: eipRenderAddress,
-    },
-  });
-  const paymasterAddress = await nftGatedPAymaster.getAddress();
-  const eipNFT = await eipNftFactory.connect(signers[0]).deploy(signers[0].address, 1000, paymasterAddress);
-  const eipNFTAddress = await eipNFT.getAddress();
-  console.log("NFT address: ", eipNFTAddress);
+  // const eipNumber = 1559;
+  // const allowedEipMints = 2;
+  // const nftOwner = signers[0];
+  // const dateCreated = "2020-09-15";
+  // const eipDescription = "NFT Royalty Standard";
 
-  const eipNumber = 1559;
-  const allowedEipMints = 2;
-  const nftOwner = signers[0];
-  const dateCreated = "2020-09-15";
-  const eipDescription = "NFT Royalty Standard";
+  // const mintRes = await eipNFT.authenticatedMint(
+  //   eipNumber,
+  //   allowedEipMints,
+  //   nftOwner,
+  //   dateCreated,
+  //   eipDescription
+  // );
 
-  const mintRes = await eipNFT.authenticatedMint(
-    eipNumber,
-    allowedEipMints,
-    nftOwner,
-    dateCreated,
-    eipDescription
-  );
-
-  const mintedOwnerBalance = await eipNFT.balanceOf(signers[0].address);
-  console.log("EOA balance: ", mintedOwnerBalance);
-
-
-  /**
-   * Deploying Smart Contract Account
-   */
-  const smartAccountContractFactory = await hre.ethers.getContractFactory("SmartAccount");
-  const smartAccount = await smartAccountContractFactory.connect(signers[0]).deploy(entryPointContract);
-  const smartAccountAddress = await smartAccount.getAddress();
-  const entrypointRes = await smartAccount.entryPoint();
-
-  console.log("Smart Account deployed to: ", smartAccountAddress, " with entrypoint: ", entrypointRes);
+  // const mintedOwnerBalance = await eipNFT.balanceOf(signers[0].address);
+  // console.log("EOA balance: ", mintedOwnerBalance);
 
 
-  /**
-  * Send NFT from EOA to smart account
-  */
+  // /**
+  //  * Deploying Smart Contract Account
+  //  */
+  // const smartAccountContractFactory = await hre.ethers.getContractFactory("SmartAccount");
+  // const smartAccount = await smartAccountContractFactory.connect(signers[0]).deploy(entryPointContract);
+  // const smartAccountAddress = await smartAccount.getAddress();
+  // const entrypointRes = await smartAccount.entryPoint();
 
-  const tokenId = encodeTokenId(eipNumber, 1);
-  await eipNFT.transferFrom(signers[0].address, smartAccountAddress, tokenId);
+  // console.log("Smart Account deployed to: ", smartAccountAddress, " with entrypoint: ", entrypointRes);
 
 
-  const smartAccountBalance = await eipNFT.balanceOf(smartAccountAddress);
-  console.log("Smart Account balance: ", smartAccountBalance);
+  // /**
+  // * Send NFT from EOA to smart account
+  // */
+
+  // const tokenId = encodeTokenId(eipNumber, 1);
+  // await eipNFT.transferFrom(signers[0].address, smartAccountAddress, tokenId);
+
+
+  // const smartAccountBalance = await eipNFT.balanceOf(smartAccountAddress);
+  // console.log("Smart Account balance: ", smartAccountBalance);
+  // console.log(`Starting balance: ${await hre.ethers.provider.getBalance(signers[0].address)}`);
 
 };
+
+const wait = async (blocks: number, hre: HardhatRuntimeEnvironment) => {
+  let currentBlock = await hre.ethers.provider.getBlockNumber();
+  while (currentBlock + blocks > (await hre.ethers.provider.getBlockNumber())) {
+    sleep(1000);
+  }
+}
+
+const sleep = async (ms: number) => {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 
 const encodeTokenId = (eipNumber: number, tokenNumber: number) => {
@@ -91,3 +109,6 @@ const encodeTokenId = (eipNumber: number, tokenNumber: number) => {
 export default func;
 func.id = "deploy_contracts"; // id required to prevent reexecution
 func.tags = [""];
+
+
+
