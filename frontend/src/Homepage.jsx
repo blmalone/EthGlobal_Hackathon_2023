328ca98c0f2461b636af802821c01e5ec5fdf4f4
Vikram Baid <vik@smartadvancedtech.com>
2023-09-23 20:57:44 -0400

    fix changes

--- frontend/src/Homepage.jsx
+++ frontend/src/Homepage.jsx
@@ -1,7 +1,5 @@
 import Login from "./Login";
 import NftInteraction from "./NftInteraction";
-import CustomButton from "./WalletConnectButton";
-import { useWeb3Modal } from "@web3modal/react";
 import React, { Component } from "react";
 
 class Homepage extends Component {
