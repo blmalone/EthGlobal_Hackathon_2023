2a56d060130bb18c9a82a0e5b650651d106c3df9
Vikram Baid <vik@smartadvancedtech.com>
2023-09-24 07:06:23 -0400

    Change

--- backend/index.js
+++ backend/index.js
@@ -46,6 +46,21 @@ app.get('/nft', async (req, res) => {
   }
 });
 
+app.get('/nft-alchemy', async (req, res) => {
+  const address = req.query.address;
+  const nftContract = req.query.contractAddress;
+  const options = { method: 'GET', headers: { accept: 'application/json' } };
+
+  const response = await axios.get(`https://polygon-mumbai.g.alchemy.com/nft/v3/F9oZzn4hiBlbbfAMtL9BCPIJ-R7yK8HG/getNFTsForOwner?owner=${address}&contractAddresses[]=${nftContract}&withMetadata=true&pageSize=100`);
+  const responseData = response.data;
+  if (responseData && responseData.ownedNfts && responseData.ownedNfts.length > 0) {
+    return res.send({
+      owned: true,
+    })
+  }
+  res.send(responseData);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
